6f10cd5f4907b97aff3dd5ce7741bf352c7375ac
Mukul <mukulphoughat@gmail.com>
2022-07-22 12:20:40 +0530

    first commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,7 @@ import {motion} from "framer-motion";
 
 export default function Home(){
     return (
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ type: "just", stiffness: 100, delay: 0.5}} className={"flex-col w-full h-full align-middle items-center"}>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ type: "just", stiffness: 100, delay: 0.5}} className={"flex-col w-full h-full dark:bg-gray-900 align-middle items-center"}>
             <Intro />
         </motion.div>
     )
--- src/Pages/Intro.js
+++ src/Pages/Intro.js
@@ -5,11 +5,12 @@ export default function Intro() {
     return (
         <motion.div className={"flex-col w-full items-center align-middle"}>
 
-            <img src={profile}
-                 className={"h-72 w-72 align-middle border-4 border-amber-300 shadow-xl shadow-amber-200 my-8 justify-self-center rounded-full mx-auto"}
+            <motion.img src={profile}
+                 whileHover={{scale: 1.1}}
+                 className={"h-72 w-72 dark:shadow-none dark:border-blue-400 align-middle border-4 border-amber-300 shadow-xl shadow-amber-200 my-8 justify-self-center rounded-full mx-auto"}
                  alt={"profile"}/>
             <div className={"mx-auto w-1/2 my-20"}>
-                <motion.p className={"font-mono mx-4 text-2xl w-full"} align={"center"}>
+                <motion.p className={"font-mono dark:text-white mx-4 text-2xl w-full"} align={"center"}>
                     Hello My Name is Mukul.<br/>I am a Front End Web Developer.<br/>
                     I have a passion for creating web applications.
                 </motion.p>
