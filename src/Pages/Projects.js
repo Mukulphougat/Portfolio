d1c084a55ab04f4fc6cea55ce352935a6a49959a
Mukul <mukulphoughat@gmail.com>
2022-07-23 14:41:43 +0530

    first commit

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -8,9 +8,9 @@ export default function Projects(){
                 <motion.div initial={{x: -750, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}} className={"flex-col align-middle p-4 w-full"}>
                     <div className={"flex-col w-full"}>
                         <h1 className={"font-mono dark:text-green-500 font-bold text-2xl w-full"}>Music APP</h1>
-                        <div className={"flex my-4 w-full"}>
-                            <img src={app1} className={"w-4/12 h-50"} alt={"Music APP"} />
-                            <motion.div initial={{x: -1950, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}} className={"text-xl mx-20 dark:text-green-400 font-mono"}>
+                        <div className={"flex flex-col sm:flex-row my-4 w-full"}>
+                            <img src={app1} className={"sm:w-4/12 sm:h-64 w-full h-36"} alt={"Music APP"} />
+                            <motion.div initial={{x: -1950, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}} className={"text-xl sm:mx-20 my-2 w-full text-justify dark:text-green-400 font-mono"}>
                                 This is a music app where we can upload songs and play them.<br/>
                                 And We can also add our own songs. And we can also delete our songs.<br/>
                                 And we can also add to favorites. And we can also delete from favorites.<br/>
