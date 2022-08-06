a2a5c62e07cf0c7fe3c4290cb260d1fde0e921c8
Mukul <mukulphoughat@gmail.com>
2022-08-06 22:10:26 +0530

    first commit

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,8 +1,21 @@
 import {motion} from "framer-motion";
+import gmailImg from "./gmail.png"
 
 export default function Contact(){
     return (
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ type: "just", stiffness: 100, delay: 0.5}} className="text-3xl h-screen font-bold underline">
-            <h1>Contact</h1>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ type: "just", stiffness: 100, delay: 0.5}} className={"text-3xl mx-20 flex flex-col h-screen"}>
+            <div className={"flex self-center my-10 flex-row dark:shadow-gray-500 justify-between dark:text-green-500 font-mono h-24 my-2 w-fit rounded-xl shadow-sm shadow-cyan-200 items-center justify-center"}>
+                <img src={gmailImg} alt={"Mail"}/>
+                <a href={"mailto:mukulphoughat@gmail.com"}><h1 className={"font-mono white mx-4 underline hover:no-underline"}>mukulphoughat@gmail.com</h1></a>
+            </div>
+            <div className={"flex self-center my-10 flex-row dark:shadow-gray-500 dark:text-green-500 font-mono h-24 my-2 w-fit rounded-xl shadow-sm shadow-cyan-200 items-center justify-center"}>
+                Phone
+            </div>
+            <div className={"flex self-center my-10 flex-row dark:shadow-gray-500 dark:text-green-500 font-mono h-24 my-2 w-fit rounded-xl shadow-sm shadow-cyan-200 items-center justify-center"}>
+                Github
+            </div>
+            <div className={"flex self-center my-10 flex-row dark:shadow-gray-500 dark:text-green-500 font-mono h-24 my-2 w-fit rounded-xl shadow-sm shadow-cyan-200 items-center justify-center"}>
+                LinkedIn
+            </div>
         </motion.div>)
 }
--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -1,7 +1,7 @@
 import {Link} from "react-router-dom";
 import logos from "../meetup.png";
 import "./comps.css"
-import {motion} from "framer-motion";
+import {AnimatePresence, motion} from "framer-motion";
 import about from "./info.png";
 import pImage from "./projects.png";
 import cImage from "./contact.png";
@@ -9,16 +9,19 @@ import resume from "./resume.png";
 import github from "./github.png";
 export default function Nav(){
     return(
-        <div className="flex-col dark:bg-gray-900 fixed bg-white top-0 h-full w-10 align-middle justify-between">
+        <AnimatePresence>
+            <div className="flex-col dark:bg-gray-900 fixed bg-white top-0 h-full w-10 align-middle justify-between">
 
-            <Link to={"/"} className="text-2xl mx-auto fixed top-2 hover:bg-opacity-50 "><motion.img whileTap={{ scale: 0.8 }} src={logos} className={"w-10 h-10"} alt={"logo"}/></Link>
-            <div className={"flex-col mx-1 fixed bottom-5 justify-center align-middle my-1"}>
-                <Link to={"/about"}><motion.div whileTap={{ scale: 0.8 }} className="my-8"><img src={about} alt={"about"} className={"w-8 h-8"}/></motion.div></Link>
-                <Link to={"/projects"}><motion.div whileTap={{ scale: 0.8 }} className="my-8"><img src={pImage} alt={"project"} className={"w-8 h-8"}/></motion.div></Link>
-                <Link to={"/contact"}><motion.div whileTap={{ scale: 0.8 }} className="my-8"><img src={cImage} alt={"contact"} className={"w-8 h-8"}/></motion.div></Link>
-                <motion.div whileTap={{ scale: 0.8 }} className="my-8"><a href={"https://drive.google.com/file/d/1BmgNfhjxUkyFJQdw1QX2DVrBlJgzt_gn/view?usp=sharing"} target={"_blank"} rel={"noreferrer"} ><img src={resume} className={"w-8 h-8"} alt={"resume"}/></a></motion.div>
-                <motion.div whileTap={{ scale: 0.8 }} ><a href={"https://github.com/Mukulphougat"} target={"_blank"} rel={"noreferrer"} ><img src={github} className={"w-8 h-8"} alt={"resume"}/></a></motion.div>
+                <Link to={"/"} className="text-2xl mx-auto fixed top-2 hover:bg-opacity-50 "><motion.img whileTap={{ scale: 0.8 }} src={logos} className={"w-10 h-10"} alt={"logo"}/></Link>
+                <div className={"flex-col mx-1 fixed bottom-5 justify-center align-middle my-1"}>
+                    <Link to={"/about"}><motion.div initial={{ y: -500}} animate={{ y:0 }} exit={{ y: -500 }} whileTap={{ scale: 0.8 }} className="my-8"><img src={about} alt={"about"} className={"w-8 h-8"}/></motion.div></Link>
+                    <Link to={"/projects"}><motion.div initial={{ y: -500}} animate={{ y:0 }} exit={{ y: -500 }} whileTap={{ scale: 0.8 }} className="my-8"><img src={pImage} alt={"project"} className={"w-8 h-8"}/></motion.div></Link>
+                    <Link to={"/contact"}><motion.div initial={{ y: -500}} animate={{ y:0 }} exit={{ y: -500 }} whileTap={{ scale: 0.8 }} className="my-8"><img src={cImage} alt={"contact"} className={"w-8 h-8"}/></motion.div></Link>
+                    <motion.div initial={{ y: -500}} animate={{ y:0 }} exit={{ y: -500 }} whileTap={{ scale: 0.8 }} className="my-8"><a href={"https://drive.google.com/file/d/1X7vsL3gbr0-VLRFHY3T-bmFOXl5YcGYe/view?usp=sharing"} target={"_blank"} rel={"noreferrer"} ><img src={resume} className={"w-8 h-8"} alt={"resume"}/></a></motion.div>
+                    <motion.div initial={{ y: -500}} animate={{ y:0 }} exit={{ y: -500 }} whileTap={{ scale: 0.8 }} ><a href={"https://github.com/Mukulphougat"} target={"_blank"} rel={"noreferrer"} ><img src={github} className={"w-8 h-8"} alt={"resume"}/></a></motion.div>
+                </div>
             </div>
-        </div>
+        </AnimatePresence>
+
     )
 }
