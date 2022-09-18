a628e8ea30c119f6d2e350e3a990dc1e1438caeb
Mukul <mukulphoughat@gmail.com>
2022-09-18 17:54:57 +0530

    first commit

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,12 +1,21 @@
 import {motion} from "framer-motion";
 import link from "./link.png";
-
+import graphql from "./Icons/graphql.228x256.png";
+import express from "./Icons/icons8-express-js-50.png";
+import java from "./Icons/icons8-java-48.png";
+import js from "./Icons/icons8-javascript-48.png";
+import mongo from "./Icons/icons8-mongodb-48.png";
+import reactIcon from "./Icons/icons8-react-80.png";
+import springBoot from "./Icons/spring.256x256.png";
+import mysqlIcon from "./Icons/icons8-mysql-48.png";
+import reduxIcon from "./Icons/redux-original.256x244.png";
+import tailwindIcon from "./Icons/tailwind-css.256x154.png";
 export default function About() {
     return (
         <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}
                     transition={{type: "just", stiffness: 100, delay: 0.5}} className="w-full h-full font-bold">
-            <div className={"flex-col my-1 rounded-xl my-2 dark:shadow-gray-700 mx-20 shadow-sm shadow-cyan-200 items-center justify-center"}>
-                <motion.h1 className={"font-mono dark:text-white font-bold p-4 underline text-3xl w-full "}>Education</motion.h1>
+            <div className={"flex-col my-1 rounded-xl my-2 mx-20 items-center justify-center"}>
+                <motion.h1 className={"font-mono uppercase font-thin dark:text-white p-4 text-3xl w-full "}>Education</motion.h1>
                 <div className={"mx-4"}>
                     <div className={'flex w-full align-middle'}>
                         <div className={"text-2xl dark:text-green-500 font-mono my-auto"}>Chandigarh University</div>
@@ -17,42 +26,45 @@ export default function About() {
                     <div className={"text-sm dark:text-green-500 font-mono"}>2021-2023</div>
                 </div>
             </div>
-            <div className={"flex-col my-8 dark:shadow-gray-700 rounded-xl mx-20 shadow-sm shadow-cyan-200 items-center justify-center"}>
-                <h1 className={"font-mono dark:text-white font-bold p-4 underline text-3xl w-full "}>Skills</h1>
-                <div className={"mx-4 flex-col align-middle justify-center "}>
-                    <motion.h1 initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
-                               className={"font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}>Java
-                    </motion.h1>
-                    <motion.h1 initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
-                               className={"font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}>Javascript
-                    </motion.h1>
-                    <motion.h1 initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
-                               className={"font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}>MongoDb
-                    </motion.h1>
-                    <motion.h1 initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
-                               className={"font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}>MySQL
-                    </motion.h1>
-                    <motion.h1 initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
-                               className={"font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}>React
-                    </motion.h1>
-                    <motion.h1 initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
-                               className={"font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}>Spring
-                    </motion.h1>
-                    <motion.h1 initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
-                               className={"font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}>Express
-                    </motion.h1>
+            {/*<div className={"flex-col h-0 my-8 rounded-xl mx-20 items-center justify-center"}>*/}
+            {/*    <hr className={"border-green-500"}/>*/}
+            {/*</div>*/}
+            <div className={"flex-col my-8 rounded-xl mx-20 items-center justify-center"}>
+                <h1 className={"font-mono dark:text-white font-thin p-4 uppercase text-3xl w-full "}>Skills</h1>
+                <div className={"mx-4 sm:grid-cols-4 grid grid-cols-3 align-middle justify-center "}>
+                    <motion.img src={java} alt={"JAVA"} initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
+                               className={"font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}/>
+                    <motion.img src={js} alt={"JavaScript"} initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
+                               className={"font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}/>
+                    <motion.img src={mongo} alt={"MongoDb"} initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
+                               className={"font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}/>
+                    <motion.img src={mysqlIcon} alt={"MySqlIcon"} initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
+                               className={"font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}/>
+                    <motion.img src={reactIcon} alt={"ReactIcon"} initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
+                               className={"w-14 h-14 font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}/>
+                    <motion.img src={springBoot} alt={"SpringBoot"} initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
+                               className={"w-14 h-14 font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}/>
+                    <motion.img src={express} alt={"ExpressIcon"} initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
+                               className={"font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}/>
+                    <motion.img src={reduxIcon} alt={"ReduxIcon"} initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
+                                className={"w-14 h-14 font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}/>
+                    <motion.img src={graphql} alt={"GraphQlIcon"} initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
+                                className={"w-14 h-14 font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}/>
+                    <motion.img src={tailwindIcon} alt={"TailwindIcon"} initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
+                                className={"w-16 h-14 font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}/>
                 </div>
             </div>
-            <div className={"flex-col my-8 dark:shadow-gray-700 rounded-xl mx-20 shadow-sm shadow-cyan-200 items-center justify-center"}>
-                <h1 className={"font-mono dark:text-white font-bold p-4 underline text-3xl w-full "}>Coding Profiles</h1>
+
+            <div className={"flex-col my-8 rounded-xl mx-20 items-center justify-center"}>
+                <h1 className={"font-mono dark:text-white font-thin uppercase p-4 text-3xl w-full "}>Coding Profiles</h1>
                 <div className={"mx-4 flex-col align-middle justify-center "}>
                     <motion.h1 initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
-                               className={"font-mono dark:text-green-500 underline hover:no-underline font-thin p-1.5 w-fit text-2xl text-blue-500"}>
-                        <a href={"https://leetcode.com/mukulphougat/"} target={"_blank"} rel={"noreferrer"}>Leetcode</a>
+                               className={"font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}>
+                        <a href={"https://leetcode.com/mukulphougat/"} target={"_blank"} rel={"noreferrer"} className={"uppercase uppercase text-blue-500 underline"}>Leetcode</a>
                     </motion.h1>
                     <motion.h1 initial={{x: -550, overflow: "hidden"}} animate={{x: 0}} transition={{delay: .8}}
-                               className={"font-mono dark:text-green-500 underline hover:no-underline font-thin p-1.5 w-fit text-2xl text-blue-500"}>
-                        <a href={"https://auth.geeksforgeeks.org/user/mukulphoughat/practice/"} target={"_blank"} rel={"noreferrer"}>GeeksForGeeks</a>
+                               className={"font-mono dark:text-green-500 font-thin p-1.5 w-fit text-2xl text-blue-500"}>
+                        <a href={"https://auth.geeksforgeeks.org/user/mukulphoughat/practice/"} target={"_blank"} rel={"noreferrer"} className={"uppercase text-blue-500 underline"}>GeeksForGeeks</a>
                     </motion.h1>
 
                 </div>
--- src/Pages/Intro.js
+++ src/Pages/Intro.js
@@ -2,20 +2,27 @@ import profile from "../profile.JPG"
 import {motion} from "framer-motion";
 
 export default function Intro() {
+    const myName = "<Mukul Phougat />";
     return (
-        <motion.div className={"flex-col w-full items-center align-middle"}>
+        <motion.div className={"flex-col flex justify-center items-center"}>
+            <div
+                className={"absolute border my-52 [#333333] mt-52 animate-ping align-middle rounded-full w-[180px] h-[180px] mx-auto my-10 justify-self-center"}/>
+            <div
+                className={"absolute border my-52 [#333333] mt-52 animate-ping align-middle rounded-full w-[200px] h-[200px] mx-auto my-10 justify-self-center"}/>
+            <div
+                className={"absolute border my-52 [#333333] mt-52 animate-ping align-middle rounded-full w-[220px] h-[220px] mx-auto my-10 justify-self-center"}/>
 
-            <motion.img src={profile}
-                 whileHover={{scale: 1.1}}
-                 className={"h-72 w-72 dark:shadow-none dark:border-blue-400 align-middle border-4 border-amber-300 shadow-xl shadow-amber-200 my-8 justify-self-center rounded-full mx-auto"}
-                 alt={"profile"}/>
-            <div className={"mx-auto w-1/2 my-20"}>
-                <motion.p className={"font-mono dark:text-white mx-4 text-2xl w-full"} align={"center"}>
-                    Hello My Name is Mukul.<br/>I am a Front End Web Developer.<br/>
-                    I have a passion for creating web applications.
-                </motion.p>
+            <div className={"relative my-52 flex justify-center align-middle items-center"}>
+                <img src={profile}
+                     className={"h-[220px] w-[220px] justify-self-center rounded-full"}
+                     alt={"profile"}/>
+                {/*<div className={"mx-auto w-full my-20"}>*/}
+                {/*    <motion.p className={"font-mono dark:text-white mx-4 text-2xl w-full"} align={"center"}>*/}
+                {/*        Hello My Name is Mukul*/}
+                {/*    </motion.p>*/}
+                {/*</div>*/}
             </div>
-
+            <h1 className={"absolute bottom-12 sm:self-center text-white dark:text-blue-900 text-2xl font-mono"}>Hello My Name Is <span className={"dark:text-green-500"}>{myName}</span></h1>
         </motion.div>
     )
 }
