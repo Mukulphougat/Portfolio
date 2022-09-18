87f51f401b1e293a5c8e4b34568a279dca1614e0
Mukul <mukulphoughat@gmail.com>
2022-09-18 19:00:33 +0530

    first commit

--- src/Pages/Intro.js
+++ src/Pages/Intro.js
@@ -18,11 +18,6 @@ export default function Intro() {
                 <img src={profile}
                      className={"h-[220px] w-[220px] justify-self-center rounded-full"}
                      alt={"profile"}/>
-                {/*<div className={"mx-auto w-full my-20"}>*/}
-                {/*    <motion.p className={"font-mono dark:text-white mx-4 text-2xl w-full"} align={"center"}>*/}
-                {/*        Hello My Name is Mukul*/}
-                {/*    </motion.p>*/}
-                {/*</div>*/}
             </div>
             <h1 className={"absolute bottom-[100px] sm:self-center text-white text-2xl font-mono dark:text-green-500"}>{myName}</h1>
             <div className={"absolute flex justify-center flex-row bottom-10 sm:self-center text-white dark:text-blue-900 text-2xl font-mono"}>
