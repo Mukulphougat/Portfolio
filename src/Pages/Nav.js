9b798177fe3e5ae1372698b1b0ef3615845aaa29
Mukul <mukulphoughat@gmail.com>
2022-09-19 14:45:59 +0530

    first commit

--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -10,15 +10,14 @@ import github from "./github.png";
 export default function Nav(){
     return(
         <AnimatePresence>
-            <div className="flex-col dark:bg-gray-900 fixed bg-white top-0 h-full w-10 align-middle justify-between">
+            <div className="flex flex-row dark:bg-gray-900 top-0 bg-white h-14 w-full align-middle">
 
-                <Link to={"/"} className="text-2xl mx-auto fixed top-2 hover:bg-opacity-50 "><motion.img whileTap={{ scale: 0.8 }} src={logos} className={"w-10 h-10"} alt={"logo"}/></Link>
-                <div className={"flex-col mx-1 fixed bottom-5 justify-center align-middle my-1"}>
-                    <Link to={"/about"}><motion.div initial={{ y: -500}} animate={{ y:0 }} exit={{ y: -500 }} whileTap={{ scale: 0.8 }} className="my-8"><img src={about} alt={"about"} className={"w-8 h-8"}/></motion.div></Link>
-                    <Link to={"/projects"}><motion.div initial={{ y: -500}} animate={{ y:0 }} exit={{ y: -500 }} whileTap={{ scale: 0.8 }} className="my-8"><img src={pImage} alt={"project"} className={"w-8 h-8"}/></motion.div></Link>
-                    {/*<Link to={"/contact"}><motion.div initial={{ y: -500}} animate={{ y:0 }} exit={{ y: -500 }} whileTap={{ scale: 0.8 }} className="my-8"><img src={cImage} alt={"contact"} className={"w-8 h-8"}/></motion.div></Link>*/}
-                    <motion.div initial={{ y: -500}} animate={{ y:0 }} exit={{ y: -500 }} whileTap={{ scale: 0.8 }} className="my-8"><a href={"https://drive.google.com/file/d/1dRvfo0Iy1PtaEOu8HR8iCVzu_gThLWL-/view?usp=sharing"} target={"_blank"} rel={"noreferrer"} ><img src={resume} className={"w-8 h-8"} alt={"resume"}/></a></motion.div>
-                    <motion.div initial={{ y: -500}} animate={{ y:0 }} exit={{ y: -500 }} whileTap={{ scale: 0.8 }} ><a href={"https://github.com/Mukulphougat"} target={"_blank"} rel={"noreferrer"} ><img src={github} className={"w-8 h-8"} alt={"resume"}/></a></motion.div>
+                <Link to={"/"} className="text-2xl self-center hover:bg-opacity-50 "><motion.img whileTap={{ scale: 0.8 }} src={logos} className={"w-10 h-10"} alt={"logo"}/></Link>
+                <div className={"flex flex-row mx-auto self-center align-middle"}>
+                    <Link to={"/about"}><motion.div initial={{ y: -500}} animate={{ y:0 }} exit={{ y: -500 }} whileTap={{ scale: 0.8 }} className={"mx-4"}><img src={about} alt={"about"} className={"w-8 h-8"}/></motion.div></Link>
+                    <Link to={"/projects"}><motion.div initial={{ y: -500}} animate={{ y:0 }} exit={{ y: -500 }} whileTap={{ scale: 0.8 }} className={"mx-4"}><img src={pImage} alt={"project"} className={"w-8 h-8"}/></motion.div></Link>
+                    <motion.div initial={{ y: -500}} animate={{ y:0 }} exit={{ y: -500 }} whileTap={{ scale: 0.8 }} className={"mx-4"}><a href={"https://drive.google.com/file/d/1dRvfo0Iy1PtaEOu8HR8iCVzu_gThLWL-/view?usp=sharing"} target={"_blank"} rel={"noreferrer"} ><img src={resume} className={"w-8 h-8"} alt={"resume"}/></a></motion.div>
+                    <motion.div initial={{ y: -500}} animate={{ y:0 }} exit={{ y: -500 }} whileTap={{ scale: 0.8 }} className={"mx-4"}><a href={"https://github.com/Mukulphougat"} target={"_blank"} rel={"noreferrer"} ><img src={github} className={"w-8 h-8"} alt={"resume"}/></a></motion.div>
                 </div>
             </div>
         </AnimatePresence>
